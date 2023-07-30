9316249b49ee4e96760aa642d4282e25431e1113
LittleRey <34397448+LittleRey@users.noreply.github.com>
2023-07-30 12:39:10 +0800

    Update get-cn-v6.py

--- python/get-cn-v6.py
+++ python/get-cn-v6.py
@@ -1,29 +1,23 @@
 import requests
+from datetime import datetime
 
-def convert_format(url):
-    response = requests.get(url)
-    if response.status_code != 200:
-        print("Failed to fetch data from the URL.")
-        return
+url = "https://ispip.clang.cn/all_cn_ipv6.txt"
+output = f"#更新时间 {datetime.now().strftime('%Y/%m/%d %I:%M:%S %p')}\n#来源 {url}\npayload：\n"
 
-    lines = response.text.strip().split('\n')
-    payload_lines = ['payload:']
-    for line in lines:
-        line = line.strip()
-        payload_lines.append(f'  - IP-CIDR6,{line},no-resolve')
+try:
+    r = requests.get(url)
+    if r.status_code == 200:
+        lines = r.text.splitlines()
+        for line in lines:
+            output += f"  - IP-CIDR6,{line},no-resolve\n"
 
-    return '\n'.join(payload_lines)
+        with open('cn-ipv6.txt', 'w') as file:
+            file.write(output)
 
-def save_to_file(file_name, content):
-    with open(file_name, 'w') as file:
-        file.write(content)
-
-if __name__ == "__main__":
-    url = "https://ispip.clang.cn/all_cn_ipv6.txt"
-    formatted_payload = convert_format(url)
-
-    if formatted_payload:
-        save_to_file("CN-V6.txt", formatted_payload)
-        print("Successfully saved to 'CN-V6.txt'.")
+        print("文件ip.txt已生成，包含格式化后的IPv6地址列表。")
+    else:
+        print("无法获取URL内容。")
+except requests.exceptions.RequestException as e:
+    print("发生错误：", e)
 
 
