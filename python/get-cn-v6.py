193b0d13613c674c90fdce38609602fda56b3828
LittleRey <34397448+LittleRey@users.noreply.github.com>
2023-07-30 12:19:32 +0800

    Update get-cn-v6.py

--- python/get-cn-v6.py
+++ python/get-cn-v6.py
@@ -14,7 +14,16 @@ def convert_format(url):
 
     return '\n'.join(payload_lines)
 
+def save_to_file(file_name, content):
+    with open(file_name, 'w') as file:
+        file.write(content)
+
 if __name__ == "__main__":
     url = "https://ispip.clang.cn/all_cn_ipv6.txt"
     formatted_payload = convert_format(url)
-    print(formatted_payload)
+
+    if formatted_payload:
+        save_to_file("CN-V6.txt", formatted_payload)
+        print("Successfully saved to 'CN-V6.txt'.")
+
+
