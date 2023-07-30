31f661543324f840d6ddeebe207cdf7e040566d9
LittleRey <34397448+LittleRey@users.noreply.github.com>
2023-07-30 12:44:16 +0800

    Create get-cn-v4.py

---
+++ python/get-cn-v4.py
@@ -0,0 +1,21 @@
+import requests
+from datetime import datetime
+
+url = "https://ispip.clang.cn/all_cn_ipv4.txt"
+output = f"#更新时间 {datetime.now().strftime('%Y/%m/%d %I:%M:%S %p')}\n#来源 {url}\npayload：\n"
+
+try:
+    r = requests.get(url)
+    if r.status_code == 200:
+        lines = r.text.splitlines()
+        for line in lines:
+            output += f"  - IP-CIDR6,{line},no-resolve\n"
+
+        with open('cn-ipv4.txt', 'w') as file:
+            file.write(output)
+
+        print("文件ip.txt已生成，包含格式化后的IPv6地址列表。")
+    else:
+        print("无法获取URL内容。")
+except requests.exceptions.RequestException as e:
+    print("发生错误：", e)
