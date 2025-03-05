55fbae9f8f24380ee0fed20b3854334d3ff7f6c6
LittleRey <34397448+LittleRey@users.noreply.github.com>
2025-03-05 15:02:58 +0800

    Update XPTV-Plugin.py

--- XPTV-Plugin.py
+++ XPTV-Plugin.py
@@ -8,7 +8,7 @@ r = requests.get(url).text
 output = f"# 更新时间 {datetime.now().strftime('%Y/%m/%d %I:%M:%S %p')}\n# 来源 {url}\npayload:\n"
 
 # 替换文本中的 ',' 为 ', '，并将多余的 ', ,' 替换为 ','
-output += r.replace(' ', '').replace('，', ',').replace(', ', ',').replace(',,', ',').replace(', DIRECT', '').replace(',DIRECT', '').replace('DOMAIN', '   - DOMAIN').replace('DOMAIN-SUFFIX', '   - DOMAIN-SUFFIX').replace('DOMAIN-KEYWORD', '   - DOMAIN-KEYWORD').replace('IP-CIDR', '   - IP-CIDR').replace('IP-CIDR6', '   - IP-CIDR6')
+output += r.replace('[Rule]', '').replace(' ', '').replace('，', ',').replace(', ', ',').replace(',,', ',').replace(', DIRECT', '').replace(',DIRECT', '').replace('DOMAIN', '   - DOMAIN').replace('DOMAIN-SUFFIX', '   - DOMAIN-SUFFIX').replace('DOMAIN-KEYWORD', '   - DOMAIN-KEYWORD').replace('IP-CIDR', '   - IP-CIDR').replace('IP-CIDR6', '   - IP-CIDR6')
 
 # 保存结果到文件
 with open('rules/XPTV-Plugin.list', 'w') as file:
