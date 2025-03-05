c8ef63dc042bccd1784824e6533f2991e28d99ab
LittleRey <34397448+LittleRey@users.noreply.github.com>
2025-03-05 12:15:16 +0800

    Create XTPV-clash.py

---
+++ XTPV-clash.py
@@ -0,0 +1,15 @@
+import requests
+from datetime import datetime
+
+url = "https://raw.githubusercontent.com/fangkuia/XPTV/refs/heads/main/X/clash.yaml"
+r = requests.get(url).text
+
+# 生成头部信息
+output = f"# 更新时间 {datetime.now().strftime('%Y/%m/%d %I:%M:%S %p')}\n# 来源 {url}\n"
+
+# 替换文本中的 ',' 为 ', '，并将多余的 ', ,' 替换为 ','
+output += r.replace(', ', ',').replace(',,', ',')
+
+# 保存结果到文件
+with open('XPTV.list', 'w') as file:
+    file.write(output)
