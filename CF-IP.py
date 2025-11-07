9d601ebe27fcd862388b2590ffa689f8df8d56e8
LittleRey <34397448+LittleRey@users.noreply.github.com>
2025-11-07 23:32:29 +0800

    Update CF-IP.py

--- CF-IP.py
+++ CF-IP.py
@@ -27,7 +27,8 @@ CF_DOMAINS = [
 ]
 
 # Cloudflare IP匹配（需要替换的）
-CF_IP_PATTERN = r"server:\s*(?:\d{1,3}\.){3}\d{1,3}"
+# CF_IP_PATTERN = r"server:\s*(?:\d{1,3}\.){3}\d{1,3}"
+CF_IP_PATTERN = r"server:\s*(?:\d{1,3}\.){3}\d{1,3}|server:\s*[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+"
 
 # 1. 抓取订阅
 resp = requests.get(SUB_URL, timeout=10)
