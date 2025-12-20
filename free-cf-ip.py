0a1b54672af34070b5b7a7911c81c758555833c0
LittleRey <34397448+LittleRey@users.noreply.github.com>
2025-12-20 23:31:42 +0800

    Update free-cf-ip.py

--- free-cf-ip.py
+++ free-cf-ip.py
@@ -21,7 +21,7 @@ GIST_TOKEN = os.getenv("GIST_TOKEN")  # Gist Token 从环境变量读取
 #china.bilibiliapp.cn
 #download.yunzhongzhuan.com
 CF_DOMAINS = [
-    ("yunzhongzhuan.com", "CF1"),
+    ("bestcf.030101.xyz", "CF1"),
     ("testingcf.jsdelivr.net", "CF2"),
 
 ]
