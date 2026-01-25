ea075d57f8dbc7920ea90e569f6643f8ff8ed5a3
LittleRey <34397448+LittleRey@users.noreply.github.com>
2026-01-25 16:21:36 +0800

    Update peiqian-raw.py

--- peiqian-raw.py
+++ peiqian-raw.py
@@ -5,7 +5,7 @@ import requests
 RAW_FILENAME = "test.yaml"   # Gist 中保存的文件名
 
 # 👉 直接写订阅链接
-SUB_URL = "https://url.v1.mk/sub?target=clash&url=https%3A%2F%2Fdash.pqjc.site%2Fapi%2Fv1%2Fclient%2Fsubscribe%3Ftoken%3D75942e964b247fe81482867a46a66bf8%26flag%3Dmeta&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FLittleRey%2Fclash-yaml%2Fmain%2Fnewname.ini&append_type=true&emoji=true&list=true&udp=true&expand=true&new_name=true&append_type=false&sort=true"
+SUB_URL = "https://url.v1.mk/sub?target=clash&url=https%3A%2F%2Fxn--cp3a08l.com%2Fapi%2Fv1%2Fclient%2Fsubscribe%3Ftoken%3D311c632a6975869aa5ca37d01ff4d4ef%26flag%3Dmeta&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FLittleRey%2Fclash-yaml%2Fmain%2Fnewname.ini&append_type=true&emoji=true&list=true&udp=true&expand=true&new_name=true&append_type=false&sort=true"
 
 # 👉 Gist 配置（可以直接写死，也可以用环境变量）
 GIST_ID = os.getenv("GIST_ID", "你的gist id")
