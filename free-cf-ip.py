b900bd41c651c8b55a028f5fdcee3450009ea7ec
LittleRey <34397448+LittleRey@users.noreply.github.com>
2026-01-29 22:28:54 +0800

    Update free-cf-ip.py

--- free-cf-ip.py
+++ free-cf-ip.py
@@ -29,7 +29,7 @@ GIST_TOKEN = os.getenv("GIST_TOKEN")  # Gist Token 从环境变量读取
 #www.shopify.com
 #cdn.yiso.fun
 CF_DOMAINS = [
-    ("cf.090227.xyz", "CF1"),
+    ("proxy.buildtest.site", "CF1"),
 ]
 
 
