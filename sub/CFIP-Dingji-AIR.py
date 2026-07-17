d3d7082fa68dd31988ace799169c288a7b8ed315
LittleRey <34397448+LittleRey@users.noreply.github.com>
2026-07-18 06:10:40 +0800

    Update CFIP-Dingji-AIR.py

--- sub/CFIP-Dingji-AIR.py
+++ sub/CFIP-Dingji-AIR.py
@@ -31,7 +31,7 @@ GIST_TOKEN = os.getenv("GIST_TOKEN")  # Gist Token 从环境变量读取
 #cdn.yiso.fun
 #("youxuan.cf.090227.xyz"
 CF_DOMAINS = [
-    ("ips.333600.xyz", "CF2"),
+    ("youxuan.cf.090227.xyz", "CF2"),
 
 ]
 
